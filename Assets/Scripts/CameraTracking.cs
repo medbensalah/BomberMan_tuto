fc87124c7db824fa0192e34e6404e08dcb962d0b
Mohamed Ben Salah <bensalahmohamed33@gmail.com>
2024-09-21 14:52:01 +0100

    Video 2

---
+++ Assets/Scripts/CameraTracking.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CameraTracking : MonoBehaviour
+{
+    [SerializeField] private Transform _player;
+    [SerializeField] private Vector2 _xClamp;
+    [SerializeField] private Vector2 _yClamp;
+    
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector2 pos = _player.position;
+        float newX = Mathf.Clamp(pos.x, _xClamp.x, _xClamp.y);
+        float newY = Mathf.Clamp(pos.y, _yClamp.x, _yClamp.y);
+        transform.position = new Vector3(newX, newY, transform.position.z);
+    }
+}
